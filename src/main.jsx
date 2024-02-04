2cf089b088030b624fd7f1b66392486e659b21d0
Chenqi <1002652154@qq.com>
2024-02-04 12:54:56 +0100

    add clear button

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,6 @@ import './main.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div id="note" contentEditable onInput={event => localStorage.setItem('note', event.target.innerHTML)} dangerouslySetInnerHTML={{ __html: localStorage.getItem('note') }}></div>
+    <button onClick={event => { localStorage.setItem('note', event.target.previousSibling.innerHTML = ''); event.preventDefault(); }}>clear</button>
   </React.StrictMode>
 );
